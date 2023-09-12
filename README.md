d1a07b3291f9e293eb617ed506547c8ba22cf33d
rileyesther <144009597+rileyesther@users.noreply.github.com>
2023-09-12 12:46:02 +0200

    Last Commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-> Fork deze leertaak en ga aan de slag, de instructie vind je in: [docs/INSTRUCTIONS.md](docs/INSTRUCTIONS.md)
+> _Fork_ deze deeltaak en ga aan de slag. De instructie vind je in: [docs/INSTRUCTIONS.md](docs/INSTRUCTIONS.md)
 
 ## Licentie
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+document.addEventListener('DOMContentLoaded', function () {
+    const profiles = document.querySelectorAll('.profile');
+    profiles.forEach(function (profile) {
+        profile.addEventListener('click', function () {
+            const popup = document.getElementById('popup1');
+            popup.style.display = 'block';
+        });
+    });
 
-function expand () {
-  document.body.classList.toggle('expand')
-}
+    
+    const closePopupElement = document.getElementById('closePopup');
+    closePopupElement.addEventListener('click', function () {
+        const popup = document.getElementById('popup1');
+        popup.style.display = 'none';
+    });
+});
